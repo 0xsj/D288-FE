4d2bea7ae73b72f8c12cfb0c62aa7692e1a613ee
sjtommylee <sjtommylee@gmail.com>
2023-09-28 12:12:45 -0700

    chore: commit save point

--- day1/HelloWorld.java
+++ day1/HelloWorld.java
@@ -5,6 +5,16 @@ public class HelloWorld {
     float hours = 40;
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int myNumber = 1;
+
+        while (myNumber != 100) {
+            if ((myNumber % 2) == 0) {
+                myNumber++;
+            } else {
+                myNumber++;
+                System.out.println(myNumber);
+            }
+        }
+
     }
 }
