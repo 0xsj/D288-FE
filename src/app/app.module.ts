ec87075112da2b49646bc869bbe4a1310446f9fb
sjtommylee <sjtommylee@gmail.com>
2024-02-08 20:14:09 -0800

    additional scaffolding, ready to go

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { LoaderComponent } from './shared/loader/loader.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    ButtonComponent,
+    LoaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
